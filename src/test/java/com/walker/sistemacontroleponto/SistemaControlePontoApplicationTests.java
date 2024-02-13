fea9600f70bf19e7ab1a8bba26b46102eca02613
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-02-13 15:32:49 -0300

    Repositório criado

---
+++ src/test/java/com/walker/sistemacontroleponto/SistemaControlePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.sistemacontroleponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControlePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
